63bcefa7ec6d2532f8f3eb805cd261f2062c9e9e
Ryan Giordano <rgiordan@gmail.com>
2025-04-30 16:03:06 -0700

    funcationality working

--- UCBGrading/examples/examples.R
+++ UCBGrading/examples/examples.R
@@ -22,8 +22,19 @@ hw_cols <- GetMatchingEntries(names(grades), "homework_[0-9]+$")
 hw_max_cols <- GetMatchingEntries(names(grades), "homework_[0-9]+_max$")
 
 
-ComputeWeightedMean(grades, quiz_cols, "quiz_mean")
+bind_cols(
+  grades["id"],
+  grades %>%
+    NormalizeColsByCols(quiz_cols, "quiz_max") %>%
+    DropLowest(paste0(quiz_cols, "_norm"), num_drops=1, new_prefix="quiz_dropped_") %>%
+    ComputeWeightedMean(paste0("quiz_dropped_", 1:3), "quiz_mean"),
+  grades %>%
+    NormalizeColsByCols(hw_cols, hw_max_cols) %>%
+    DropLowest(paste0(hw_cols, "_norm"), num_drops=1, new_prefix="hw_dropped_") %>%
+    ComputeWeightedMean(paste0("hw_dropped_", 1:2), "hw_mean")
+)
+
 ComputeWeightedMean(grades, quiz_cols, "quiz_mean", weights=1:4)
-NormalizeColsByNumber(grades, quiz_cols, 10)
 DropLowest(grades, hw_cols, num_drops=1, new_prefix="hw_dropped_")
-NormalizeColsByCols(grades, hw_cols, hw_max_cols)
+
+NormalizeColsByCols(grades, quiz_cols, "quiz_max")
